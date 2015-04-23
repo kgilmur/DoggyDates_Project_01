c70836088ed2249f4ed0abf839a94d90d4629043
kgilmur <kgilmur@gmail.com>
2015-04-23 16:39:37 -0700

    finallllllllll

--- models/user.js
+++ models/user.js
@@ -9,7 +9,6 @@ module.exports = function(sequelize, DataTypes) {
      type:DataTypes.STRING,
      validate:{
        isEmail:{
-         args:true,
          msg:"Please enter a valid email address."
        }
      }
